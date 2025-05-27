5c0677bbdfb4d47b68ced11dfa97a93d0f053f08
dineth panditha <92966321+dinethh@users.noreply.github.com>
2025-05-27 16:39:04 +0530

    all done V1 ok

--- DreamDay/obj/Debug/net8.0/DreamDay.AssemblyInfo.cs
+++ DreamDay/obj/Debug/net8.0/DreamDay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DreamDay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d31614032060c31f4ffe615c43be6dce1c8d142e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c32e5cb138b48f6c11bd0b19468923db34c33e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DreamDay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DreamDay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
